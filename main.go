3dd2a3e1e7b308b7c8979e68a39b450d51cbfc97
Dirk Olbrich <mail@dirkolbrich.de>
2017-05-31 21:58:30 +0200

    fetch crumb from yahoo finance

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -14,142 +15,185 @@ import (
 	"golang.org/x/net/html"
 )
 
-// scrap the necassary json from the yahoo finance page
-func getJson(symbol string) (crumb string) {
+var (
+	httpClient *http.Client
+)
+
+// Historical fetches the latest historical quotes from finance.yahoo.com
+func Historical(symbol, start, end, interval, event string) string {
+	var crumbURL = "https://finance.yahoo.com/quote/{symbol}/history?p={symbol}"
+	var historyURL = "https://query1.finance.yahoo.com/v7/finance/download/{symbol}?"
+	var configURL = "period1={start}&period2={end}&interval={interval}&events={event}&crumb={crumb}"
+
+	// validate symbol
 	if symbol == "" {
-		log.Println("No symbol provided")
+		log.Println("no symbol provided")
 		return ""
 	}
 
-	// basic url
-	var url = strings.Replace("https://finance.yahoo.com/quote/symbol/history?p=symbol", "symbol", symbol, -1)
+	// query crumb url
+	url := strings.Replace(crumbURL, "{symbol}", symbol, -1)
+	crumb, _ := getCrumb(url)
 
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Println(err)
+	start, end = orderDates(start, end)
+	start, end = parseDates(start, end)
+
+	// modify configURL
+	historyURL = strings.Replace(historyURL, "{symbol}", symbol, -1)
+	configURL = strings.Replace(configURL, "{start}", start, -1)
+	configURL = strings.Replace(configURL, "{end}", end, -1)
+	configURL = strings.Replace(configURL, "{interval}", interval, -1)
+	configURL = strings.Replace(configURL, "{event}", event, -1)
+	configURL = strings.Replace(configURL, "{crumb}", crumb, -1)
+
+	queryURL := historyURL + configURL
+	data, _ := readCSVFromUrl(queryURL)
+	fmt.Printf("%T %#v\n", data, data)
+
+	for id, row := range data {
+		if id == 10 {
+			break
+		}
+		fmt.Printf("id:%v %T %q\n", id, row, row)
 	}
-	defer resp.Body.Close()
+
+	return historyURL + configURL
+}
+
+func readCSVFromUrl(url string) ([][]string, error) {
+	r, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	reader := csv.NewReader(r.Body)
+	data, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// getCrumb scraps the neccessary json anf cookie from the yahoo finance page and returns the single crumb string
+func getCrumb(url string) (crumb string, cookies []*http.Cookie) {
+	// fetch yahoo finance for basic page
+	r, err := http.Get(url)
+	if err != nil {
+		log.Println("Could not query page", err)
+	}
+
+	defer r.Body.Close()
+	fmt.Printf("%T %v\n", r.Body, r.Body)
+	for _, c := range r.Cookies() {
+		fmt.Printf("%T %#v\n", c, c)
+	}
+	// collect the send cookies
+	cookies = r.Cookies()
+
+	// Tokenize html
+	z := html.NewTokenizer(r.Body)
 
 	var js string
-	// Tokenize html
-	z := html.NewTokenizer(resp.Body)
+L:
 	for {
 		tt := z.Next()
 		switch tt {
 		case html.ErrorToken:
 			// End of the document, we're done
-			return
+			break
 		// find a start token
 		case html.StartTagToken:
 			// get tag name of token and find <script> token
 			tag, _ := z.TagName()
 			if string(tag) == "script" {
-				// forward to next token and check if it is a text token
+				// script tag found, forward to next token and check if it is a text token
 				token := z.Next()
+				// token is text, not EndTagToken
 				if token == html.TextToken {
-					// select the token
-					data := z.Token()
-					// match data for reactjs "root.App.main"
-					matched, _ := regexp.MatchString("root.App.main", data.Data)
-					if matched {
-						re := regexp.MustCompile(`root.App.main\s+=\s+(?P<json>\{.*\})`)
-						matches := re.FindStringSubmatch(data.Data)
-						// map named groups
-						groups := make(map[string]string)
-						for i, name := range re.SubexpNames() {
-							if i != 0 {
-								groups[name] = matches[i]
-							}
-						}
-						js = groups["json"]
-						return js
+					data := parseToken("root.App.main", z.Token())
+					if len(data) > 0 {
+						js = data
+						break L
 					}
 				}
 			}
 		}
 	}
-	return js
-}
 
-// get the crumb cookie from
-func getCrumb(raw string) (crumb string) {
+	// search the data string for CrumbStore
+	j := regexText(`(?P<CrumbStore>"CrumbStore"\s?:\s?(?P<crumb>{"crumb"\s?:\s?".*?"}))`, js)
+	// select the CrumbStore group
+	crumbStore := j["crumb"]
+
+	// parsing json
 	var f interface{}
-	err := json.Unmarshal([]byte(raw), &f)
+	err = json.Unmarshal([]byte(crumbStore), &f)
 	if err != nil {
-		log.Println(err)
+		log.Println("invalid json", err)
 	}
 
-	// walk through json map - what a pain in the ar**
+	// walk through json map
 	m := f.(map[string]interface{})
-	context := m["context"].(map[string]interface{})
-	dispatcher := context["dispatcher"].(map[string]interface{})
-	stores := dispatcher["stores"].(map[string]interface{})
-	crumbStore := stores["CrumbStore"].(map[string]interface{})
+	crumb = m["crumb"].(string)
 
-	// fmt.Printf("crumb: %v\n", crumbStore["crumb"])
-	// for k, v := range crumbStore {
-	// 	fmt.Printf("key: %v calue: %v\n", k, v)
-	// }
-
-	return crumbStore["crumb"].(string)
+	return crumb, cookies
 }
 
-func Historical(symbol, start, end, param string) string {
+// parseToken parses a html.Token if it contains a string
+func parseToken(s string, t html.Token) string {
+	var data string
+	// match data for string
+	matched, _ := regexp.MatchString(s, t.Data)
+	if matched {
+		// found string in Data
+		data = t.Data
+	}
 
-	var baseURL = "http://ichart.finance.yahoo.com/table.csv?s="
-	var configValue = "&ignore=.csv"
-
-	start, end = orderDates(start, end)
-	var dateValue = parseDates(start, end)
-
-	paramValue := "&g=d"
-
-	queryURL := baseURL + symbol + dateValue + paramValue + configValue
-
-	return queryURL
+	return data
 }
 
-// parse start and end date for query
-func parseDates(start, end string) string {
+// regexText searches the given string for the regular expression, returns a map
+func regexText(regex string, s string) map[string]string {
+	re := regexp.MustCompile(regex)
+	matches := re.FindStringSubmatch(s)
+
+	// rearrange submatches to map
+	m := make(map[string]string)
+	for i, name := range re.SubexpNames() {
+		if i != 0 {
+			m[name] = matches[i]
+		}
+	}
+
+	return m
+}
+
+// parseDates parses start and end date to UNIX time string
+func parseDates(start, end string) (s, e string) {
 	start, end = orderDates(start, end)
 
-	// yahoo fincnae formats the query with
-	// a: start month
-	// b: start day
-	// c: start year
-	// d: end month
-	// e: end day
-	// f: end year
-	var startDate, endDate string
-
-	if start == "" {
-		startDate = "&a=&b=&c="
+	if len(start) == 0 {
+		// set to min
+		s = "0"
 	} else {
 		date, _ := time.Parse("2006-01-02", start)
-		// yahoo finance index for month starts with 0
-		sMonth := "&a=" + strconv.Itoa(int(date.Month())-1)
-		sDay := "&b=" + strconv.Itoa(date.Day())
-		sYear := "&c=" + strconv.Itoa(date.Year())
-		startDate = sMonth + sDay + sYear
-		fmt.Println(startDate)
+		s = strconv.Itoa(int(date.Unix()))
 	}
 
-	if end == "" {
-		endDate = "&d=&e=&f="
+	if len(end) == 0 {
+		// set to max
+		e = "9999999999"
 	} else {
 		date, _ := time.Parse("2006-01-02", end)
-		// yahoo finance index for month starts with 0
-		eMonth := "&d=" + strconv.Itoa(int(date.Month())-1)
-		eDay := "&e=" + strconv.Itoa(date.Day())
-		eYear := "&f=" + strconv.Itoa(date.Year())
-		endDate = eMonth + eDay + eYear
-		fmt.Println(endDate)
+		e = strconv.Itoa(int(date.Unix()))
 	}
 
-	return startDate + endDate
+	return s, e
 }
 
-// order dates - start must be earlier than end
+// orderDates validates the correct order of start to end - start must be earlier than end
 func orderDates(start, end string) (string, string) {
 	if (start > end) && (end != "") {
 		tmp := start
@@ -162,35 +206,15 @@ func orderDates(start, end string) (string, string) {
 
 // define symbol flags
 var symbol = flag.String("symbol", "", "stock symbol e.g. bas.de")
-
-// var startDate = flag.String("start", "", "start date in format yyyy-mm-dd")
-// var endDate = flag.String("end", "", "start date in format yyyy-mm-dd")
-// var interval = flag.String("interval", "1d", "param for query type, e.g. d for daily quotes")
+var start = flag.String("start", "", "start date in format yyyy-mm-dd")
+var end = flag.String("end", "", "start date in format yyyy-mm-dd")
+var interval = flag.String("interval", "1d", "param for query interval, e.g. d for daily, weekly or monthly")
+var event = flag.String("event", "history", "param for query type, e.g. d for history, div or split")
 
 func main() {
 	flag.Parse()
-	fmt.Println(*symbol)
+	fmt.Println(*symbol, *start, *end)
 
-	js := getJson(*symbol)
-	crumb := getCrumb(js)
-	fmt.Printf("%T %v %s\n", crumb, len(crumb), crumb)
-
-	// s, e := orderDates(*startDate, *endDate)
-	// println(s, e)
-
-	// url := Historical(*symbol, *startDate, *endDate, *param)
-	// fmt.Println(url)
-
-	// url := "http://example.org/"
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	// handle error
-	// }
-	// defer resp.Body.Close()
-
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	// handle error
-	// }
-	// fmt.Printf("%s", body)
+	url := Historical(*symbol, *start, *end, *interval, *event)
+	fmt.Printf("%q", url)
 }
---
+++ main_test.go
@@ -0,0 +1,44 @@
+package yahoofinancequery
+
+// test html generated from finance.yahoo.com
+var testHTML = "
+    <!DOCTYPE html>
+    <html>
+    <head">
+        <meta charset="utf-8">
+        <title>title</title>
+        <meta name="keywords" content="test">
+        <meta name="description" lang="en-US" content="yahoo finance test html">
+    </head>
+
+    <body>
+        <div id="app">
+            <p>test paragraph</p>
+        </div>
+        <script>
+            (function (root) {
+                /* -- Data -- */
+                root.App || (root.App = {});
+                root.App.main = {
+                  "context": {
+                    "dispatcher": {
+                      "stores": {
+                        "FlyoutStore": {},
+                        "CrumbStore": {
+                          "crumb": "fCv.rYUxTML"
+                        },
+                        "HeaderStore": {}
+                      }
+                    },
+                    "options": {}
+                  },
+                  "plugins": {}
+                };
+            }(this));
+        </script>
+    </body>
+    </html>
+"
+
+
+
