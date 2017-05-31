8968edc43afdffc027fcbca2649b4cef440ee2e0
Dirk Olbrich <mail@dirkolbrich.de>
2017-06-01 00:21:28 +0200

    finalize historical query

--- main.go
+++ main.go
@@ -2,38 +2,43 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
-
-	"golang.org/x/net/html"
 )
 
-var (
-	httpClient *http.Client
-)
+type Quote struct {
+	Date     string
+	Symbol   string
+	Open     string
+	High     string
+	Low      string
+	Close    string
+	AdjClose string
+	Volume   string
+}
 
 // Historical fetches the latest historical quotes from finance.yahoo.com
-func Historical(symbol, start, end, interval, event string) string {
+func Historical(symbol, start, end string) (quotes []Quote) {
 	var crumbURL = "https://finance.yahoo.com/quote/{symbol}/history?p={symbol}"
 	var historyURL = "https://query1.finance.yahoo.com/v7/finance/download/{symbol}?"
-	var configURL = "period1={start}&period2={end}&interval={interval}&events={event}&crumb={crumb}"
+	var configURL = "period1={start}&period2={end}&interval=1d&events=history&crumb={crumb}"
 
 	// validate symbol
-	if symbol == "" {
+	if len(symbol) == 0 {
 		log.Println("no symbol provided")
-		return ""
+		return quotes
 	}
 
 	// query crumb url
 	url := strings.Replace(crumbURL, "{symbol}", symbol, -1)
-	crumb, _ := getCrumb(url)
+	crumb, cookies := getCrumb(url)
 
 	start, end = orderDates(start, end)
 	start, end = parseDates(start, end)
@@ -42,32 +47,45 @@ func Historical(symbol, start, end, interval, event string) string {
 	historyURL = strings.Replace(historyURL, "{symbol}", symbol, -1)
 	configURL = strings.Replace(configURL, "{start}", start, -1)
 	configURL = strings.Replace(configURL, "{end}", end, -1)
-	configURL = strings.Replace(configURL, "{interval}", interval, -1)
-	configURL = strings.Replace(configURL, "{event}", event, -1)
 	configURL = strings.Replace(configURL, "{crumb}", crumb, -1)
 
 	queryURL := historyURL + configURL
-	data, _ := readCSVFromUrl(queryURL)
-	fmt.Printf("%T %#v\n", data, data)
 
-	for id, row := range data {
-		if id == 10 {
-			break
-		}
-		fmt.Printf("id:%v %T %q\n", id, row, row)
+	// query for csv
+	data, err := readCSVFromURL(queryURL, cookies)
+	if err != nil {
+		log.Fatal("Could not establish new request: ", err)
 	}
 
-	return historyURL + configURL
+	quotes = parseHistoricalCSV(symbol, data)
+
+	return quotes
 }
 
-func readCSVFromUrl(url string) ([][]string, error) {
-	r, err := http.Get(url)
+func readCSVFromURL(url string, cookies []*http.Cookie) ([][]string, error) {
+	// set client
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	// define request
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	defer r.Body.Close()
+	for _, c := range cookies {
+		req.AddCookie(c)
+	}
 
-	reader := csv.NewReader(r.Body)
+	// issue rewuest
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// read csv from response
+	reader := csv.NewReader(resp.Body)
 	data, err := reader.ReadAll()
 	if err != nil {
 		return nil, err
@@ -76,98 +94,86 @@ func readCSVFromUrl(url string) ([][]string, error) {
 	return data, nil
 }
 
-// getCrumb scraps the neccessary json anf cookie from the yahoo finance page and returns the single crumb string
-func getCrumb(url string) (crumb string, cookies []*http.Cookie) {
-	// fetch yahoo finance for basic page
-	r, err := http.Get(url)
-	if err != nil {
-		log.Println("Could not query page", err)
-	}
-
-	defer r.Body.Close()
-	fmt.Printf("%T %v\n", r.Body, r.Body)
-	for _, c := range r.Cookies() {
-		fmt.Printf("%T %#v\n", c, c)
-	}
-	// collect the send cookies
-	cookies = r.Cookies()
-
-	// Tokenize html
-	z := html.NewTokenizer(r.Body)
-
-	var js string
-L:
-	for {
-		tt := z.Next()
-		switch tt {
-		case html.ErrorToken:
-			// End of the document, we're done
-			break
-		// find a start token
-		case html.StartTagToken:
-			// get tag name of token and find <script> token
-			tag, _ := z.TagName()
-			if string(tag) == "script" {
-				// script tag found, forward to next token and check if it is a text token
-				token := z.Next()
-				// token is text, not EndTagToken
-				if token == html.TextToken {
-					data := parseToken("root.App.main", z.Token())
-					if len(data) > 0 {
-						js = data
-						break L
-					}
-				}
+func parseHistoricalCSV(symbol string, data [][]string) (quotes []Quote) {
+	var csvHeader []string
+	// parse csv to map
+	for id, row := range data {
+		if id == 0 {
+			csvHeader = row
+			continue
+		}
+		if id < 10 {
+			// rearange row to header
+			quote := make(map[string]string)
+			for i, v := range row {
+				quote[csvHeader[i]] = v
 			}
+
+			// create new Quote and populate
+			q := Quote{}
+			q.Date = quote["Date"]
+			q.Symbol = strings.ToUpper(symbol)
+			q.Open = quote["Open"]
+			q.High = quote["High"]
+			q.Low = quote["Low"]
+			q.Close = quote["Close"]
+			q.AdjClose = quote["Adj Close"]
+			q.Volume = quote["Volume"]
+
+			quotes = append(quotes, q)
 		}
 	}
 
-	// search the data string for CrumbStore
-	j := regexText(`(?P<CrumbStore>"CrumbStore"\s?:\s?(?P<crumb>{"crumb"\s?:\s?".*?"}))`, js)
-	// select the CrumbStore group
-	crumbStore := j["crumb"]
+	return
+}
 
-	// parsing json
-	var f interface{}
-	err = json.Unmarshal([]byte(crumbStore), &f)
+// getCrumb scraps the neccessary json and cookie from the yahoo finance page and returns the crumb string with the cookies
+func getCrumb(url string) (crumb string, cookies []*http.Cookie) {
+	// set client
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	// define request
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Println("invalid json", err)
+		log.Fatal("Could not establish new request: ", err)
+	}
+	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Could not query page: ", err)
+	}
+	defer resp.Body.Close()
+
+	// collect the send cookies
+	cookies = resp.Cookies()
+
+	// read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Could not read response body: ", err)
 	}
 
-	// walk through json map
-	m := f.(map[string]interface{})
-	crumb = m["crumb"].(string)
-
-	return crumb, cookies
-}
-
-// parseToken parses a html.Token if it contains a string
-func parseToken(s string, t html.Token) string {
-	var data string
-	// match data for string
-	matched, _ := regexp.MatchString(s, t.Data)
-	if matched {
-		// found string in Data
-		data = t.Data
+	// search for crumb in body
+	re := regexp.MustCompile(`(?P<CrumbStore>"CrumbStore"\s?:\s?{"crumb"\s?:\s?"(?P<crumb>.*?)"})`)
+	matches := re.FindStringSubmatch(string(body))
+	if matches == nil {
+		log.Println("Could not find crumb")
 	}
-
-	return data
-}
-
-// regexText searches the given string for the regular expression, returns a map
-func regexText(regex string, s string) map[string]string {
-	re := regexp.MustCompile(regex)
-	matches := re.FindStringSubmatch(s)
 
 	// rearrange submatches to map
-	m := make(map[string]string)
+	matchMap := make(map[string]string)
 	for i, name := range re.SubexpNames() {
 		if i != 0 {
-			m[name] = matches[i]
+			matchMap[name] = matches[i]
 		}
 	}
+	crumb = matchMap["crumb"]
 
-	return m
+	return crumb, cookies
 }
 
 // parseDates parses start and end date to UNIX time string
@@ -178,7 +184,10 @@ func parseDates(start, end string) (s, e string) {
 		// set to min
 		s = "0"
 	} else {
-		date, _ := time.Parse("2006-01-02", start)
+		date, err := time.Parse("2006-01-02", start)
+		if err != nil {
+			log.Fatal("Could not parse string to time: ", err)
+		}
 		s = strconv.Itoa(int(date.Unix()))
 	}
 
@@ -186,7 +195,10 @@ func parseDates(start, end string) (s, e string) {
 		// set to max
 		e = "9999999999"
 	} else {
-		date, _ := time.Parse("2006-01-02", end)
+		date, err := time.Parse("2006-01-02", end)
+		if err != nil {
+			log.Fatal("Could not parse string to time: ", err)
+		}
 		e = strconv.Itoa(int(date.Unix()))
 	}
 
@@ -208,13 +220,11 @@ func orderDates(start, end string) (string, string) {
 var symbol = flag.String("symbol", "", "stock symbol e.g. bas.de")
 var start = flag.String("start", "", "start date in format yyyy-mm-dd")
 var end = flag.String("end", "", "start date in format yyyy-mm-dd")
-var interval = flag.String("interval", "1d", "param for query interval, e.g. d for daily, weekly or monthly")
-var event = flag.String("event", "history", "param for query type, e.g. d for history, div or split")
 
 func main() {
 	flag.Parse()
-	fmt.Println(*symbol, *start, *end)
+	// fmt.Println(*symbol, *start, *end)
 
-	url := Historical(*symbol, *start, *end, *interval, *event)
-	fmt.Printf("%q", url)
+	quotes := Historical(*symbol, *start, *end)
+	fmt.Printf("%#v", quotes)
 }
